2e46b7805e5932f5fcdab09c2e4e0773bf282e3a
Manuel Marín Figueredo <manuelmarinfigueredo@gmail.com>
2024-11-28 16:39:17 +0100

    ControladorRobot terminado

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
@@ -0,0 +1,35 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.Objects;
+
+public class ControladorRobot {
+
+    private Robot robot;
+
+    public ControladorRobot(Robot robot){
+        Objects.requireNonNull(robot, "El robot no puede ser nulo.");
+        this.robot = new Robot(robot);
+    }
+
+    public Robot getRobot(){
+        return new Robot(robot);
+    }
+
+    public void ejecutar(char comando) throws OperationNotSupportedException {
+
+        switch (Character.toUpperCase(comando)) {
+            case 'A':
+                robot.avanzar();
+                break;
+            case 'D':
+                robot.girarALaDerecha();
+                break;
+            case 'I':
+                robot.girarALaIzquierda();
+                break;
+            default:
+                throw new OperationNotSupportedException("Comando desconocido.");
+        }
+    }
+}
